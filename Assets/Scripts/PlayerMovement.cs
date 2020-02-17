0eb1a91c56429166467eaf45c14762344c45afe6
kika2001 <47228282+kika2001@users.noreply.github.com>
2020-02-17 17:36:41 +0000

    l

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,13 +15,25 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask groundMask;
     Vector3 velocity;
     bool isGrounded;
-
+    bool isWall;
+    public Transform WallCheck;
+    public float WallDistance = 0.4f;
+    public LayerMask wallMask;
+    private bool iswallrunning = false;
+    private bool flag = false;
     private void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if (isGrounded && velocity.y <0)
-        {
+        isWall = Physics.CheckSphere(WallCheck.position, WallDistance, wallMask);
+        if (isGrounded && velocity.y < 0)
+        {            
             velocity.y = -2f;
+        }
+        if (isGrounded || !isWall)
+        {
+            iswallrunning = false;
+            flag = false;
+            
         }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -35,5 +47,31 @@ public class PlayerMovement : MonoBehaviour
         }
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+        //Parkour andar na parede
+        if (isWall && !isGrounded)
+        {
+            iswallrunning = true;
+        }
+        if (iswallrunning)
+        {
+            Vector3 direction = new Vector3(1,1,1);
+            if (flag==false)
+            {
+                Debug.Log(velocity.y);
+                velocity.y = 2f + velocity.y;
+                Debug.Log(velocity.y);
+                flag = true;
+                direction = new Vector3();
+                direction.Normalize();
+            }
+            transform.position += direction * 4 * Time.deltaTime;
+            velocity.y = velocity.y + 0.01f;
+            Debug.Log(Time.deltaTime);
+            //Debug.Log(velocity.y);
+        }
+        Debug.Log(gameObject.transform.position.y);
+        //Debug.Log("Está na parede:"+isWall);
+        //Debug.Log("Está no chao:" + isGrounded);
+        //Debug.Log(iswallrunning);
     }
 }
