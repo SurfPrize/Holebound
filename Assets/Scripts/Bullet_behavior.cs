db1ef2665c091ce253fe3b9c30ed57acb9debd3a
SurfPrize <manueljosmo@hotmail.com>
2020-02-17 22:42:55 +0000

    Revert "ola"
    This reverts commit 21e17c2fe88ad04277c8d0fd2c8b127e1e4e8b86.

--- Assets/Scripts/Bullet_behavior.cs
+++ Assets/Scripts/Bullet_behavior.cs
@@ -9,35 +9,19 @@ public class Bullet_behavior : NetworkBehaviour
 {
 
     private Rigidbody rb;
-
-    [SerializeField]
-    private GameObject buracoPrefab;
-
-    private NetworkIdentity player;
-
-    private NetworkIdentity ni;
+    
+    private Shoot_bullet father;
 
 
     // Start is called before the first frame update
     private void Start()
     {
-
-        player = transform.root.GetComponent<NetworkIdentity>();
-        ni = GetComponent<NetworkIdentity>();
+        father= transform.root.GetComponent<Shoot_bullet>();
         rb = GetComponent<Rigidbody>();
         transform.parent = null;
         Destroy(gameObject, 10);
     }
-
-    private void Assingauth()
-    {
-        ni.AssignClientAuthority(player.GetComponent<NetworkIdentity>().connectionToClient);
-    }
-
-    private void Resignauth()
-    {
-        ni.RemoveClientAuthority(player.GetComponent<NetworkIdentity>().connectionToClient);
-    }
+    
 
     public void Shoot_bullet(float speed, Transform pistola)
     {
@@ -53,34 +37,10 @@ public class Bullet_behavior : NetworkBehaviour
     {
         if (collision.transform.tag != "Vortex_Ball")
         {
-            Assingauth();
-            Cmd_createhole(collision.gameObject);
-            Resignauth();
+            father.Cmd_createhole(collision.transform.position,collision.transform.rotation,gameObject, collision.transform.tag);
         }
-        Destroy(gameObject);
+        father.Cmd_Hitground(gameObject);
     }
 
-    [Command]
-    private void Cmd_createhole(GameObject parede)
-    {
-        GameObject nburaco = Instantiate(buracoPrefab, transform);
-        NetworkServer.Spawn(nburaco,player.connectionToServer);
-        NetworkServer.Spawn
-        nburaco.transform.parent = null;
-        switch (parede.transform.tag)
-        {
-            case "chao":
-                nburaco.transform.position = new Vector3(nburaco.transform.position.x, parede.transform.position.y, nburaco.transform.position.z);
-                break;
-            case "parede":
-                nburaco.transform.position = new Vector3(nburaco.transform.position.x, nburaco.transform.position.y, parede.transform.position.z);
-                break;
-            case "paredex":
-                nburaco.transform.position = new Vector3(parede.transform.position.x, nburaco.transform.position.y, nburaco.transform.position.z);
-                break;
-        }
-        nburaco.transform.localScale = Vector3.one;
-        nburaco.transform.rotation = parede.transform.rotation;
-        Destroy(nburaco, 10);
-    }
+    
 }
