1b55e78040d33ce567b007045d40e521d546e5b7
SurfPrize <manueljosmo@hotmail.com>
2020-03-24 19:05:28 +0000

    zzzz

---
+++ Assets/Scripts/Menu_cmds.cs
@@ -0,0 +1,24 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Menu_cmds : MonoBehaviour
+{
+    public void Exit_game()
+    {
+        Application.Quit();
+    }
+
+    public void Create_Room(GameObject prefab_room)
+    {
+        GameObject novo=Instantiate(prefab_room);
+        
+    }
+
+    public void Search_Room(GameObject prefab_search)
+    {
+        Instantiate(prefab_search);
+    }
+}
